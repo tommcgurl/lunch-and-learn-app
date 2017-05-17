ecbc56911a79a703a6f73221a29934c9007d6727
Tom McGurl <thomas@chartbeat.com>
2017-05-17 12:08:47 -0400

    Adding API call to mock JSON.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,49 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      inventory: {}
+    };
+  }
+
   componentWillMount() {
     fetch('http://localhost:4000/db')
       .then(response => {
         return response.json();
       })
-      .then(responsJson => {
-        debugger;
-        console.log(responsJson);
+      .then(responseJson => {
+        console.log(responseJson);
+        this.setState({
+          inventory: responseJson.inventory
+        });
+      })
+      .catch(err => {
+        console.error(err);
       });
+  }
+
+  renderInventoryWhenReady() {
+    if (this.state.inventory.kind_bars) {
+      return (
+          <div className="inventory-container" >
+            <h3 className="item-name">
+              {this.state.inventory.kind_bars.name}
+            </h3>
+            <p className="item-description">
+              {this.state.inventory.kind_bars.description}
+            </p>
+            <p className="item-votes">
+              {this.state.inventory.kind_bars.votes}
+            </p>
+          </div>
+      );
+    } else {
+      return (
+        <p className="loading-indicator"> Loading ... </p>
+      );
+    }
   }
 
   render() {
@@ -23,9 +57,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Inventory</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.renderInventoryWhenReady()}
       </div>
     );
   }
