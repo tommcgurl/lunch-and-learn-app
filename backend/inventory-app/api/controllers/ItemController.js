93c8a854e5273781ce0b7852fb381a989f37542c
Tom McGurl <thomas@chartbeat.com>
2017-11-08 12:08:31 -0500

    New API for sorted map.

--- backend/inventory-app/api/controllers/ItemController.js
+++ backend/inventory-app/api/controllers/ItemController.js
@@ -14,8 +14,8 @@ const getInventoryMap = function (items){
 };
 
 module.exports = {
-	getMap: function (req, res) {
-		Item.find({}, (err, data) =>{
+	map: function (req, res) {
+		Item.find({}, (err, data) => {
 			if (err) {
 				res.send(500, {
 					error: "Database error. All hope is lost. Abandon the surface."
@@ -25,7 +25,34 @@ module.exports = {
 			res.json(getInventoryMap(data));
 		});
   },
+	sortedMap: function(req, res) {
+		const query = Item.find()
+		query.sort('name ASC')
+		query.exec((err, data) => {
+			if (err) {
+				res.send(500, {
+					error: "Database error. All hope is lost. Abandon the surface."
+				});
+				return;
+			}
+			res.json(getInventoryMap(data));
+		});
+	},
+	sorted: function (req, res) {
+		const query = Item.find()
+		query.sort('name ASC')
+		query.exec((err, data) => {
+			if (err) {
+				res.send(500, {
+					error: "Database error. All hope is lost. Abandon the surface."
+				});
+				return;
+			}
+			res.json(data);
+		});
+	},
 	withName: function(req, res) {
+		// Get an item using it's 'name' column.
 		const name = req.query.name;
 		Item.find({name: name}, (err, data) => {
 			if (err) {
