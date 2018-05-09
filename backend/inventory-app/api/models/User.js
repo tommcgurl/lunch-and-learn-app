b77d52e647f500b814892ae3e3f3c47d8e849f92
Tom McGurl <thomas@chartbeat.com>
2018-05-09 12:10:24 -0400

    Start of setting up user creds

--- backend/inventory-app/api/models/User.js
+++ backend/inventory-app/api/models/User.js
@@ -4,11 +4,46 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+const bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
 
   attributes: {
-
+    email: {
+        type: 'email',
+        required: true,
+        unique: true
+      },
+      username: {
+        type: 'string',
+        required: true,
+        unique: true
+      },
+      password: {
+        type: 'string',
+        required: true
+      }
+    },
+    customToJSON: function() {
+       return _.omit(this, ['password'])
+    },
+    beforeCreate: function(user, cb){
+      bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+          if(err) return cb(err);
+          user.password = hash;
+          return cb();
+        });
+      });
+    },
+    beforeUpdate: function(user, cb){
+      bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+          if(err) return cb(err);
+          user.password = hash;
+          return cb();
+        });
+      });
+    },
   }
 };
-
